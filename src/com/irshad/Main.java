aa2b71c4ab66aba9b383b2969364cb83110b596d
irshad-tech <irshad3030@gmail.com>
2020-08-01 16:37:34 -0400

    irshad: Added new Number class and main class changes

--- src/com/irshad/Main.java
+++ src/com/irshad/Main.java
@@ -3,6 +3,8 @@ package com.irshad;
 public class Main {
 
     public static void main(String[] args) {
+        Number number = new Number();
+        number.getSum(10, 20);
         System.out.println("This is to test check in github");
     }
 }
---
+++ src/com/irshad/Number.java
@@ -0,0 +1,10 @@
+package com.irshad;
+
+public class Number {
+
+    public int getSum(int num1, int num2){
+        int sum = num1 + num2;
+        System.out.println("Total sum of two numbers: " + sum);
+        return sum;
+    }
+}
